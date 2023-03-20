017e87f85aaeb781d480bc622abd07edca9e8fde
RobWar-code <rowaniar@aol.com>
2023-03-20 17:38:24 +0000

    Set-up environment vars and auto deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8@7-=d-myojhz6#jfoppfnbsz($g74f4&a98)po5np4mfn%rpc'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8@7-=d-myojhz6#jfoppfnbsz($g74f4&a98)po5np4mfn%rpc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['my-django-example.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
